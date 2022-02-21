d7646aa945a8f040cbd92cb5b438bed624943771
Periklis Kyrtsis <pk@istec.de>
2022-02-21 19:43:52 +0100

    merge day3 parts into one

--- src/main/java/aoc2015/Day03.java
+++ src/main/java/aoc2015/Day03.java
@@ -16,14 +16,30 @@
 // ^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses.
 //
 // How many houses receive at least one present?
-// 2572
+
+
+// --- Part Two ---
+// The next year, to speed up the process, Santa creates a robot version of himself, Robo-Santa, to deliver presents with him.
+// 
+// Santa and Robo-Santa start at the same location (delivering two presents to the same starting house), then 
+// take turns moving based on instructions from the elf, who is eggnoggedly reading from the same script as the previous year.
+// 
+// For example:
+// 
+// ^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south.
+// ^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.
+// ^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
+//
+// This year, how many houses receive at least one present?
 
 package aoc2015;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import utils.Utils;
 
@@ -37,6 +53,11 @@ public class Day03 {
 		Map<Position, Integer> visited = new HashMap<>();
 		visited.put(currentPosition, 1);
 
+		Set<Position> set = new HashSet<>();
+		Position santaPos = new Position(0, 0);
+		Position roboPos = new Position(0, 0);
+		set.add(santaPos);
+		
 		String line;
 		try {
 			while ((line = reader.readLine()) != null) {
@@ -47,15 +68,22 @@ public class Day03 {
 					} else {
 						visited.put(currentPosition, visited.get(currentPosition) + 1);
 					}
+					
+					if (i % 2 == 1) {
+						santaPos = Day03.findNewPosition(santaPos, line.charAt(i));
+						set.add(santaPos);
+					} else {
+						roboPos = Day03.findNewPosition(roboPos, line.charAt(i));
+						set.add(roboPos);
+					}
 				}
-
-				System.out.println(visited.size());
 			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+		System.out.println("Part 1: " + visited.size());
+		System.out.println("Part 2: " + set.size());
 	}
 
 	public static Position findNewPosition(Position p, char c) {
